059080ee6d5193b05c44b068be8240bc3d0a3ded
amb258012@gmail.com <amb258012@gmail.com>
2024-01-21 22:03:15 +0900

    첫번째 커밋

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div>
-      <h3   className="title">쿠쿠후레시</h3>
+      <h3   className="title">쿠쿠후레시aaa</h3>
       <p className="title-sub">by {name}</p>
     </div>
   )
